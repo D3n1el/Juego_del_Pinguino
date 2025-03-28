26fcf06163c6f23b889a2ae8fcd5b70a7fe71f15
D3n1el <dani.alcaraz.p@gmail.com>
2025-03-28 17:39:51 +0100

    Clase JuegoVista
    Se han definido: atributo, constructor, getter, setter y métodos necesarios.
    Actualmente tiene errores porque el tipo "Partida" está definido en otra clase de otra branch

--- src/Vista/JugadorVista.java
+++ src/Vista/JugadorVista.java
@@ -1,5 +1,27 @@
 package Vista;
 
 public class JugadorVista {
-
+	
+	private Partida partida;
+	
+	public JugadorVista(Partida partida) {
+		this.partida = partida;
+	}
+	
+	public Partida getPartida() {
+		return this.partida;
+	}
+	
+	public void setPartida(Partida PARTIDA) {
+		this.partida = PARTIDA;
+	}
+	
+	public void iniciarVistaJuego() {
+		
+	}
+	
+	public void actualizarVista() {
+		
+	}
+	
 }
