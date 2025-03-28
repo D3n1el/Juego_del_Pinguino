8f729cc5244f67d0d36b41330450ad9fb752e1e3
D3n1el <dani.alcaraz.p@gmail.com>
2025-03-28 18:45:20 +0100

    Clase VistaTablero
    Se ha creado el atributo, constructor, getter, setter y métodos de la clase VistaTablero. Da error porque falta definir el tipo "Tablero" que está en otra clase de otra branch.

--- src/Vista/TableroVista.java
+++ src/Vista/TableroVista.java
@@ -1,5 +1,25 @@
 package Vista;
 
 public class TableroVista {
-
+	private Tablero tablero;
+	
+	public JugadorVista(Jugador jugador) {
+		this.tablero = tablero;
+	}
+	
+	public Tablero getPartida() {
+		return this.tablero;
+	}
+	
+	public void setPartida(Tablero JUGADOR) {
+		this.tablero = JUGADOR;
+	}
+	
+	public void mostrarTablero() {
+		
+	}
+	
+	public void actualizarTableroVista() {
+		
+	}
 }
