3f677a1254585a2c6d63352f3fcbc12de045c7ff
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2025-03-31 19:20:00 +0200

    Clases acabadas CasillaTrineo, CasilaInterrogante y Jugador
    Metodos de Casilla CasillaTrineo, CasilaInterrogante y Jugador acabadas.
    
    Atributos jugador acabados

--- src/Modelo/CasillaInterrogante.java
+++ src/Modelo/CasillaInterrogante.java
@@ -2,6 +2,10 @@ package Modelo;
 
 public class CasillaInterrogante extends Casilla {
 	
-	
+	public void activarEventoAleatorio() {
+		
+		
+	}
+
 
 }
--- src/Modelo/Jugador.java
+++ src/Modelo/Jugador.java
@@ -2,5 +2,25 @@ package Modelo;
 
 public class Jugador extends ControladorJugador{
 
+	private String idJugador;
+	private String Nombre;
+	private int PosicionAcutal;
+	private Inventario inventario;
+	private boolean esCPU;
 	
+	public void usarBolaDeNieve(Jugador jugador) {
+		
+	}
+	public void gestionarTurno() {
+		
+	}
+	public void mover(int posicion) {
+		
+	}
+	public void afectarPorCasilla (Casilla casilla) {
+		
+	}
+
 }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
