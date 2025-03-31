b503d44a0b703d85f0d057ec1444e8243399a229
oriol-valentines <oriolvalentinesmiro@gmail.com>
2025-03-31 19:39:43 +0200

    implementación
    Se han añadido los atributos, constructores, getters, setters y funciones en las distintas clases

--- src/Modelo/CPU.java
+++ src/Modelo/CPU.java
@@ -1,5 +1,35 @@
 package Modelo;
 
-public class CPU {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class CPU{
+	
+	//ATRIBUTOS
+	private String dificultad;
+	
+	//CONSTRUCTOR
+	public CPU(String dificultad) {
+		this.dificultad = dificultad;
+	}
+	
+	//GETTERS 
+	public String getDificultad() {
+		return this.dificultad;
+	}
+	
+	//SETTERS 
+	public void setDificultad(String dificultad) {
+		this.dificultad = dificultad;
+	}
+	
+	//FUNCIONES
+	public void realizarMovimiento() {
+		
+	}
+	public void determinarEstrategia() {
+		
+	}
+	
 }
--- src/Modelo/Dado.java
+++ src/Modelo/Dado.java
@@ -1,5 +1,35 @@
 package Modelo;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Dado {
 
+	//ATRIBUTO
+	private String tipo;
+	
+	//CONSTRUCTOR
+	public Dado(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	//GETTER
+	public String getTipo() {
+		return this.tipo;
+	}
+	
+	//SETTER
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	//FUNCIONES
+	
+	public int lanzar() {
+		Random r = new Random();
+		int i = r.nextInt(3) + 1;
+		
+		return i;
+	}
 }
