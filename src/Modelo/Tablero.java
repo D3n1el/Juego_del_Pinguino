55c5b6a9aab95c58f7ba6651264aa98edfcbffcd
D3n1el <dani.alcaraz.p@gmail.com>
2025-04-01 20:22:58 +0200

    Cambios y añadidos en "Tabla"
    Se han eliminado getters y setters. La clase en sí se ha vuelto subclase de TableroVista. Se han cambiado los valores del constructor y se ha añadido código en el método "generarTablero".

--- src/Modelo/Tablero.java
+++ src/Modelo/Tablero.java
@@ -2,25 +2,45 @@ package Modelo;
 
 import java.util.ArrayList;
 
-public class Tablero {
+import Controlador.ControladorTablero;
+import Vista.TableroVista;
 
-private ArrayList<Casilla> casillas;
-	//Constructor
-	public Tablero (ArrayList casillas) {
-		this.casillas = casillas;
-	}
+public class Tablero extends TableroVista {
 	
+	private int valor;
+	
+	public Tablero(int valor) {
+		super(null);
+		this.valor = valor;
+	}
+
 	//Metodos
 	public void generarTablero() {
+		ArrayList<Tablero[][]> tableros = new ArrayList<>();
 		
+		Tablero[][] tablero1 = {
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)}
+		    };
+		
+		tableros.add(tablero1);
+		
+		Tablero[][] tablero2 = {
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)},
+		        {new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1), new Tablero(1)}
+		    };
+		tableros.add(tablero2);
 	}
+	
 	
 	public void obtenerEstadoTablero() {
 		
-	}
-	
-	public ArrayList<Casilla> getCasillas() {
-	    return this.casillas;
 	}
 	
 	}
